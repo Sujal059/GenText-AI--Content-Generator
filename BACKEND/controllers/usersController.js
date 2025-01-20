9c442889f81d1b82f73495b6eb386b713799797d
Sujal059 <chsujalm@gmail.com>
2025-01-20 22:02:42 +0530

    cookies bug solved

--- BACKEND/controllers/usersController.js
+++ BACKEND/controllers/usersController.js
@@ -69,7 +69,7 @@ const login = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
+    sameSite: "none",
     // maxAge: 24 * 60 * 60 * 1000, //1 day
   });
 
